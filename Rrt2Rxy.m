7f95dd26779629a9b536d20b68f3c472623b5bef
Bryant <tangyu990628@gmail.com>
2022-06-21 15:18:04 +0200

    first

--- Rrt2Rxy.m
+++ Rrt2Rxy.m
@@ -3,11 +3,14 @@
 %         sigma_t, deviation in angle
 %         theta, relative angle between target and sensor
 
-function Rxy = Rrt2Rxy(sigma_r, sigma_t, theta)
+function Rxy = Rrt2Rxy(Rtr, theta)
+sigma_r = sqrt(Rtr(1,1));
+sigma_t = sqrt(Rtr(2,2));
 c11 = cos(theta)^2/sigma_r^2 + sin(theta)^2/sigma_r^2;
 c22 = sin(theta)^2/sigma_r^2 + cos(theta)^2/sigma_r^2;
 c12 = cos(theta)*sin(theta)*(1/sigma_r^2 - 1/sigma_t^2);
-
-Rxy = [1/c11, 1/c12;
-       1/c12, 1/c22];
+% 
+% Rxy = [1/c11, 1/c12;
+%        1/c12, 1/c22];
+Rxy=Rtr;
 end
---
+++ Rrt2Rxy_2.m
@@ -0,0 +1,15 @@
+% convert variance matrix from polar to cartesian
+% param:  sigma_r, deviation in distance (0.02d)
+%         sigma_t, deviation in angle (0.01d)
+%         theta, relative angle between target and sensor (dy/dx)
+
+function Rxy = Rrt2Rxy_2(Rrt, theta)
+%c11 = cos(theta)^2/sigma_r^2 + sin(theta)^2/sigma_r^2;
+%c22 = sin(theta)^2/sigma_r^2 + cos(theta)^2/sigma_r^2;
+%c12 = cos(theta)*sin(theta)*(1/sigma_r^2 - 1/sigma_t^2);
+%Rxy = [1/c11, 1/c12;1/c12, 1/c22];
+
+R = [cos(theta), -sin(theta); sin(theta), cos(theta)];
+Rxy = R*Rrt*R';
+
+end
